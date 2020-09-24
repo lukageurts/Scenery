d670f8d5f895183464c52733be92934a39496341
lukageurts <71170846+lukageurts@users.noreply.github.com>
2020-09-24 11:21:39 +0200

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,231 @@
+function setup() {
+  createCanvas(500,500);
+  background(61,64,91);
+
+  //tweede gebouw
+  noStroke();
+  fill(224,122,95);
+  rect(250,25,200,475);
+  
+  //tweede gebouw tweede kleur
+  noStroke();
+  fill(240,159,139);
+  rect(350,25,100,475);
+
+ //raam een gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,50,160,25);
+
+ //raam twee gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,95,160,25);
+
+ //raam drie gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,140,160,25);
+
+ //raam vier gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,185,160,25);
+
+ //raam vijf gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,230,160,25);
+
+ //raam zes gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,275,160,25);
+
+ //raam zes gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,320,160,25);
+
+ //raam zeven gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,365,160,25);
+
+ //raam acht gebouw twee
+ noStroke();
+ fill(244,241,222);
+ rect(270,410,160,25);
+
+ //balk twee gebouw een
+noStroke();
+fill(20,50);
+rect(315,380,10,150);
+
+
+//derde gebouw
+noStroke();
+fill(129,178,154);
+rect(220,380,100,350);
+
+//derde gebouw balk
+noStroke();
+fill(244,241,222);
+rect(220,380,100,12.5);
+
+//derde gebouw raam
+noStroke();
+fill(244,241,222);
+rect(220,405,90,15);
+
+//derde gebouw raam
+noStroke();
+fill(244,241,222);
+rect(220,435,90,15);
+
+//derde gebouw raam
+noStroke();
+fill(244,241,222);
+rect(220,465,90,15);
+
+//schoorsteen
+noStroke();
+fill(244,241,222);
+rect(130,110,85,100);
+
+
+//eerste gebouw
+noStroke();
+fill(242,204,143);
+rect(25,150,200,350);
+
+//raam een gebouw een
+noStroke();
+fill(244,241,222);
+rect(50,175,150,25);
+
+//raam twee gebouw een
+noStroke();
+fill(244,241,222);
+rect(50,475,150,25);
+
+//klein raam een gebouw een
+noStroke();
+fill(244,241,222);
+rect(70,350,25,25);
+
+//klein raam twee gebouw een
+noStroke();
+fill(244,241,222);
+rect(100,350,25,25);
+
+//klein raam drie gebouw een
+noStroke();
+fill(244,241,222);
+rect(130,350,25,25);
+
+//klein raam vier gebouw een
+noStroke();
+fill(244,241,222);
+rect(160,350,25,25);
+
+ //klein raam vijf gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(70,380,25,25);
+
+ //klein raam zes gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(100,380,25,25);
+
+ //klein raam zeven gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(130,380,25,25);
+
+ //klein raam acht gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(160,380,25,25);
+
+ //klein raam negen gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(70,410,25,25);
+
+ //klein raam tien gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(100,410,25,25);
+
+ //klein raam elf gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(130,410,25,25);
+
+ //klein raam twaalf gebouw een
+ noStroke();
+ fill(244,241,222);
+ rect(160,410,25,25);
+
+//balk een gebouw een
+noStroke();
+fill(244,241,222);
+rect(12.5,215,225,50);
+
+//balk twee gebouw een
+noStroke();
+fill(244,241,222);
+rect(12.5,280,225,50);
+
+
+//derde gebouw
+stroke(244,241,222);
+strokeWeight(3);
+fill(242,204,143);
+ellipse(420,305,100,80);
+
+//vierde gebouw
+noStroke();
+fill(242,204,143);
+rect(350,300,140,350);
+
+//balk twee gebouw vier
+noStroke();
+fill(244,241,222);
+rect(350,300,140,8);
+
+//balk twee gebouw vier
+noStroke();
+fill(244,241,222);
+rect(375,330,20,150);
+
+//balk twee gebouw vier
+noStroke();
+fill(244,241,222);
+rect(410,330,20,150);
+
+//balk twee gebouw vier
+noStroke();
+fill(244,241,222);
+rect(445,330,20,150);
+
+//balk twee gebouw een
+noStroke();
+fill(20,50);
+rect(225,380,10,150);
+
+//balk twee gebouw een
+noStroke();
+fill(20,50);
+rect(337,300,13,200);
+
+fill(244,241,222);
+text("Luka Geurts",20,30);
+
+
+}
+
+function draw() {
+}
